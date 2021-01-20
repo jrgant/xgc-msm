79de1bdfb92a9d0ce00f2884daa622383a12b7df
jrgant <jason.gantenberg@gmail.com>
2021-01-20 00:39:52 -0500

    convert repo to R package

--- R/calculate_targets.r
+++ R/calculate_targets.r
@@ -1,10 +1,13 @@
-raceage_grid <- function() {
-  dt <- as.data.table(expand.grid(race = c("B", "H", "O", "W"), age.grp = 1:5))
-  setkeyv(dt, c("race", "age.grp"))
-  dt
-}
+#' Calculating Target Statistics
+#'
+#' These functions extract and calculate simulated outcomes
+#' based on the calibration target statistics specified.
+#'
+#' @param dat An object of class netsim.
+#' @param tailn The number of weeks (always at the tail) to extract and average over.
 
-# Probability of being diagnosed with HIV among those with HIV, by race and age.
+#' @describeIn calculate_targets Probability of being diagnosed with HIV among those with HIV, by race and age.
+#' @export
 target_prob_hivdx_byraceage <- function(dat, tailn) {
   df <- raceage_grid()
 
@@ -27,7 +30,9 @@ target_prob_hivdx_byraceage <- function(dat, tailn) {
   })
 }
 
-# Calculate age distribution among those diagnosed with HIV, by race/ethnicity.
+#' @describeIn calculate_targets Calculate age distribution among
+#' those diagnosed with HIV, by race/ethnicity.
+#' @export
 target_prob_agedx_byrace <- function(dat, tailn) {
 
   dl <- list(
@@ -63,19 +68,18 @@ target_prob_agedx_byrace <- function(dat, tailn) {
   out
 }
 
-# Calculate probability of viral suppression among those with diagnosed HIV,
-# by race/ethnicity and age.
+#' @describeIn calculate_targets Calculate probability of viral suppression among those with diagnosed HIV, by race/ethnicity and age.
+#' @export
 target_prob_vls_byraceage <- function(dat, tailn) {
-
   vsupp.targs <- names(dat$epi)[grepl("cct", names(dat$epi))]
-
   sapply(vsupp.targs, function(x) {
     vls.prob <- tail(unlist(dat$epi[x]), n = tailn)
     mean(vls.prob)
   })
 }
 
-# Calculate probability of PrEP use among indicated agents, by race/ethnicity.
+#' @describeIn calculate_targets Calculate probability of PrEP use among indicated agents, by race/ethnicity.
+#' @export
 target_prob_prep_byrace <- function(dat, tailn) {
   r <- unique(raceage_grid()$race)
 
@@ -92,8 +96,8 @@ target_prob_prep_byrace <- function(dat, tailn) {
   })
 }
 
-# Return proportion of anatomic sites tested among MSM seeking testing at
-# the clinic.
+#' @describeIn calculate_targets Return proportion of anatomic sites tested among MSM seeking testing at the clinic.
+#' @export
 target_prop_anatsites_tested <- function(dat, tailn) {
   vars <- names(dat$epi)[grepl("^prop", names(dat$epi))]
   sapply(vars, function(x) {
@@ -101,33 +105,11 @@ target_prop_anatsites_tested <- function(dat, tailn) {
   })
 }
 
-# Return probability of being GC+ at tested anatomic sites.
+#' @describeIn calculate_targets Return probability of being GC+ at tested anatomic sites.
+#' @export
 target_prob_gcpos_tested_anatsites <- function(dat, tailn) {
   vars <- names(dat$epi)[grepl("^prob", names(dat$epi))]
   sapply(vars, function(x) {
     mean(tail(unlist(dat$epi[x]), n = tailn))
   })
-}
-
-# Helper function to use as input into the xgc() function
-# defined in burnin/abc/sim.prep.r. Intended for quick tests.
-test_xgc <- function(seed = 123, priorlist = priors) {
-  pl <- sapply(priorlist, function(x) {
-    l <- unlist(x)
-    runif(1, as.numeric(l[2]), as.numeric(l[3]))
-  })
-  c(seed, pl)
-}
-
-# Helper function to set temporary environment variable.
-setenv <- function(nsteps = 200,
-                   tail_length = 52,
-                   sims_below_tol = 50,
-                   slurm_nprocs = Sys.getenv("SLURM_NPROCS")) {
-  Sys.setenv(
-    "NSTEPS"          = nsteps,
-    "TAIL_LENGTH"     = tail_length,
-    "SIMS_BELOW_TOL"  = sims_below_tol,
-    "SLURM_NPROCS"    = slurm_nprocs
-  )
 }
---
+++ R/helpers.r
@@ -0,0 +1,42 @@
+#' Helper functions
+#'
+#' These functions provide easy access to demographic distributions
+#' in the target population as well as helper functions to generate lookup
+#' tables for different combinations of parameter levels.
+#'
+#' @import data.table
+
+#' @describeIn helpers Produce a lookup table of all possible combinations of
+#' race/ethnicity and age group.
+#' @export
+raceage_grid <- function() {
+  dt <- as.data.table(expand.grid(race = c("B", "H", "O", "W"), age.grp = 1:5))
+  setkeyv(dt, c("race", "age.grp"))
+  dt
+}
+
+
+#' @describeIn helpers Helper function to use as input into the xgc() function
+# defined in burnin/abc/sim.prep.r. Intended for quick tests.
+#' @export
+test_xgc <- function(seed = 123, priorlist = priors) {
+  pl <- sapply(priorlist, function(x) {
+    l <- unlist(x)
+    runif(1, as.numeric(l[2]), as.numeric(l[3]))
+  })
+  c(seed, pl)
+}
+
+#' @describeIn helpers Helper function to set temporary environment variable.
+#' @export
+setenv <- function(nsteps = 200,
+                   tail_length = 52,
+                   sims_below_tol = 50,
+                   slurm_nprocs = Sys.getenv("SLURM_NPROCS")) {
+  Sys.setenv(
+    "NSTEPS"          = nsteps,
+    "TAIL_LENGTH"     = tail_length,
+    "SIMS_BELOW_TOL"  = sims_below_tol,
+    "SLURM_NPROCS"    = slurm_nprocs
+  )
+}
