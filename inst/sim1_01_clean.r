a53d101ddeae17478b3dbd817fd7d5b235e4808b
jrgant <jason.gantenberg@gmail.com>
2021-04-30 19:17:13 -0400

    fix file save and rename sim1 data clean

---
+++ inst/sim1_01_clean.r
@@ -0,0 +1,38 @@
+################################################################################
+## SETUP ##
+################################################################################
+
+library(pacman)
+
+p_load(
+  data.table,
+  EpiModel,
+  ggplot2,
+  ggthemes,
+  foreach,
+  doParallel
+)
+
+
+################################################################################
+## FILES ##
+################################################################################
+
+cs <- list.files(
+  "~/scratch/sim1",
+  pattern = "rds",
+  full.names = TRUE
+)
+
+ncores <- detectCores()
+registerDoParallel(ncores)
+
+epi <- foreach(i = seq_along(cs), .combine = rbind) %dopar% {
+  cd <- readRDS(cs[i])
+  cde <- as.data.table(cd$epi)
+  cde[, simid := i]
+  cde[, at := 1:.N]
+  cde
+}
+
+saveRDS(epi, here::here("burnin", "abc", "sim1", "sim1_epi.rds"))
