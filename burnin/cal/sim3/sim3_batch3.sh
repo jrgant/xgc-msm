6879a97609d83fca3001fec4b0a3619fa8afb799
jrgant <jason.gantenberg@gmail.com>
2021-09-05 17:25:09 -0400

    commit additional batch scripts in expanded sim3

---
+++ burnin/cal/sim3/sim3_batch3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J Sim3-LHS-XGC
+#SBATCH --time=3:00:00
+#SBATCH -p batch
+#SBATCH --mem=3GB
+#SBATCH -n 1
+#SBATCH --array=2001-3000
+#SBATCH -o LHS-Sim3_ARRAY-%A_JOB-%J_SIMNO-%4a.log
+#SBATCH --export=ALL,NSIMS=1,NSTEPS=3120,ARRIVE_RATE_ADD_PER20K=1.285,SIMDIR=~/scratch/sim3
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jrgant@brown.edu
+module load R/4.0.3
+cd ~/data/jgantenb/xgcmsm/
+Rscript ./burnin/cal/sim3_02_lhs.r --vanilla
